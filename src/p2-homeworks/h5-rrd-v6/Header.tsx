a88167463613ff946a8a34eb3efb68d889dea707
Dizziby <Dizzi_by@mail.ru>
2022-04-27 22:04:11 +0300

    Add HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import styles from "./Header.module.css"
+import {PATH} from "./Pages";
 
 function Header() {
+
     return (
-        <div>
-            // add NavLinks
+        <div className={styles.header}>
+            {/*add NavLinks*/}
+            <div className={styles.navLinks}>
+                <NavLink className={({isActive}) =>
+                    isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`}
+                         to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
+                <NavLink className={({isActive}) =>
+                    isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`}
+                         to={PATH.JUNIOR}>junior</NavLink>
+                <NavLink className={({isActive}) =>
+                    isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`}
+                         to={PATH.JUNIOR_PLUS}>junior-plus</NavLink>
+                <div className={styles.square}></div>
+            </div>
 
         </div>
     )
