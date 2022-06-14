20e95a98c7e01ef31fd62c56a7cc569156da434f
Dizziby <Dizzi_by@mail.ru>
2022-06-14 23:40:58 +0300

    Added HW12

--- src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import HW13 from "../../h13/HW13";
 
 function JuniorPlus() {
     return (
         <div>
             JuniorPlus
-            {/*<HW11/>*/}
-            {/*<HW12/>*/}
+            <HW13/>
+            {/*<HW14/>*/}
         </div>
     )
 }
